d1709c969bb079c7939d1ee122e6999d6d325bb7
Kelsey Duffy <kduffy@osisoft.com>
2022-04-29 10:23:17 -0400

    upgrade to dotnet 6

--- AssetsRestApi/WaveData.cs
+++ AssetsRestApi/WaveData.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.Globalization;
 using System.Text;
 
 namespace AssetsRestApi
@@ -26,16 +27,18 @@ namespace AssetsRestApi
 
         public override string ToString()
         {
+            CultureInfo cultureInfo = CultureInfo.InvariantCulture;
+
             StringBuilder builder = new ();
-            builder.Append($"Order: {Order}");
-            builder.Append($", Radians: {Radians}");
-            builder.Append($", Tau: {Tau}");
-            builder.Append($", Sin: {Sin}");
-            builder.Append($", Cos: {Cos}");
-            builder.Append($", Tan: {Tan}");
-            builder.Append($", Sinh: {Sinh}");
-            builder.Append($", Cosh: {Cosh}");
-            builder.Append($", Tanh: {Tanh}");
+            builder.Append(cultureInfo, $"Order: {Order}");
+            builder.Append(cultureInfo, $", Radians: {Radians}");
+            builder.Append(cultureInfo, $", Tau: {Tau}");
+            builder.Append(cultureInfo, $", Sin: {Sin}");
+            builder.Append(cultureInfo, $", Cos: {Cos}");
+            builder.Append(cultureInfo, $", Tan: {Tan}");
+            builder.Append(cultureInfo, $", Sinh: {Sinh}");
+            builder.Append(cultureInfo, $", Cosh: {Cosh}");
+            builder.Append(cultureInfo, $", Tanh: {Tanh}");
             return builder.ToString();
         }
     }
